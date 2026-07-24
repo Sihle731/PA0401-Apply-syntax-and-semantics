55871b3a502088d14533b182365c88d0db2ea983
Andile Andre3000 <lasmkhwanazi@gmail.com>
2026-07-24 22:15:15 +0100

    Add files via upload

---
+++ PA0401 – Apply syntax and semantics.py
@@ -0,0 +1,11 @@
+##1
+
+name = str(input('Enter your name: '))
+city = str(input('Enter your city: '))
+
+print(f"Hello World! My name is {name} and my hometown is {city}.")
+
+##2
+number = 10
+
+print("The number is:", number)
