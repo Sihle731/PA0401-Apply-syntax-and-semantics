4e417bc350fea8c06d3048e454f196dc5dfba995
Andile Andre3000 <lasmkhwanazi@gmail.com>
2026-08-24 16:01:15 +0100

    Add files via upload

--- PA0401 – Apply syntax and semantics.py
+++ PA0401 – Apply syntax and semantics.py
@@ -1,14 +1,16 @@
 ##1
 name = "Sihle"
-print(f"Hello World! My name is {name}.")
+print(name)
 
 city = "Johannesburg"
 print(f"Hello World! My hometown is {city}.")
+
 '''console.log("Hello World);
 the line is missing a closing quote and semicolon, which will cause a syntax error. 
-It should be: console.log("Hello World");'''
-
+It should be: '''
+#console.log("Hello World"); but this is for JavaScript, not Python. In Python, we use print() function to display output.
+print("Hello World")
 
 ##2
 number = 10
-print("The number is:", number)
+print(number)
